013db33c5013fb64b8c3cfb417692901c78c5eee
Hiro Asari <asari.ruby@gmail.com>
2010-09-20 17:30:01 -0700

    Initial commit of a bare metal Rails 3 application on JRuby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Test::Application.config.secret_token = '5271d96bf1e6c7a0fa442cd636adb505db52b3be24f75cb176b5a0ba7a8de7881aaa0763f0af4ceabeec7921738b19db6f7302d34827ef50b8b9700ea1336946'
